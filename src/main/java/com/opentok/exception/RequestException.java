c27adfd51732489a382a77331211b93ead1fa539
Ankur Oberoi <aoberoi@gmail.com>
2014-04-28 06:18:39 +0200

    implement error handing for HttpClient

--- src/main/java/com/opentok/exception/RequestException.java
+++ src/main/java/com/opentok/exception/RequestException.java
@@ -8,4 +8,8 @@ public class RequestException extends OpenTokException {
         super(message);
     }
 
+    public RequestException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
 }
