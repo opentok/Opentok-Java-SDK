e2139009acea550f723a1f41211c330e860a15d3
Ankur Oberoi <aoberoi@gmail.com>
2016-02-04 01:20:48 -0800

    regenerated license headers

--- src/main/java/com/opentok/package-info.java
+++ src/main/java/com/opentok/package-info.java
@@ -1,6 +1,6 @@
 /**
  * OpenTok Java SDK
- * Copyright (C) 2015 TokBox, Inc.
+ * Copyright (C) 2016 TokBox, Inc.
  * http://www.tokbox.com
  *
  * Licensed under The MIT License (MIT). See LICENSE file for more information.
