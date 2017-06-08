31dd86a6dd66fdf36b954e80a962620e6d4ce9db
Aiham Hammami <aiham@aiham.net>
2017-06-09 00:45:24 +1000

    Update JS samples to use latest API and add error handling

--- sample/HelloWorld/public/js/helloworld.js
+++ sample/HelloWorld/public/js/helloworld.js
@@ -1,8 +1,8 @@
 // Initialize an OpenTok Session object
-var session = OT.initSession(sessionId);
+var session = OT.initSession(apiKey, sessionId);
 
 // Initialize a Publisher, and place it into the element with id="publisher"
-var publisher = OT.initPublisher(apiKey, 'publisher');
+var publisher = OT.initPublisher('publisher');
 
 // Attach an event handler for when the session dispatches the 'streamCreated' event.
 session.on('streamCreated', function(event) {
@@ -11,19 +11,27 @@ session.on('streamCreated', function(event) {
     // Subscribe to the stream that caused this event, put it inside the DOM element with id='subscribers'
     session.subscribe(event.stream, 'subscribers', {
         insertMode: 'append'
+    }, function(error) {
+        if (error) {
+            console.error('Failed to subscribe', error);
+        }
     });
 });
 
 // Connect to the Session using the 'apiKey' of the application and a 'token' for permission
-session.connect(apiKey, token, function(error) {
+session.connect(token, function(error) {
     // This function runs when session.connect() asynchronously completes
 
     // Handle connection errors
     if (error) {
-        return console.error(error);
+        console.error('Failed to connect', error);
+    } else {
+        // Publish the publisher we initialzed earlier (this will trigger 'streamCreated' on other
+        // clients)
+        session.publish(publisher, function(error) {
+            if (error) {
+                console.error('Failed to publish', error);
+            }
+        });
     }
-
-    // Publish the publisher we initialzed earlier (this will trigger 'streamCreated' on other
-    // clients)
-    session.publish(publisher);
 });
