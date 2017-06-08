bb4e791be3f8a72bef8ebbc94732b02c9c25d40c
Aiham Hammami <aiham@aiham.net>
2017-06-09 00:48:02 +1000

    Update JS style: use 2 spaces, not 4

--- sample/HelloWorld/public/js/helloworld.js
+++ sample/HelloWorld/public/js/helloworld.js
@@ -6,32 +6,32 @@ var publisher = OT.initPublisher('publisher');
 
 // Attach an event handler for when the session dispatches the 'streamCreated' event.
 session.on('streamCreated', function(event) {
-    // This function runs when another client publishes a stream (eg. session.publish())
+  // This function runs when another client publishes a stream (eg. session.publish())
 
-    // Subscribe to the stream that caused this event, put it inside the DOM element with id='subscribers'
-    session.subscribe(event.stream, 'subscribers', {
-        insertMode: 'append'
-    }, function(error) {
-        if (error) {
-            console.error('Failed to subscribe', error);
-        }
-    });
+  // Subscribe to the stream that caused this event, put it inside the DOM element with id='subscribers'
+  session.subscribe(event.stream, 'subscribers', {
+    insertMode: 'append'
+  }, function(error) {
+    if (error) {
+      console.error('Failed to subscribe', error);
+    }
+  });
 });
 
 // Connect to the Session using the 'apiKey' of the application and a 'token' for permission
 session.connect(token, function(error) {
-    // This function runs when session.connect() asynchronously completes
+  // This function runs when session.connect() asynchronously completes
 
-    // Handle connection errors
-    if (error) {
-        console.error('Failed to connect', error);
-    } else {
-        // Publish the publisher we initialzed earlier (this will trigger 'streamCreated' on other
-        // clients)
-        session.publish(publisher, function(error) {
-            if (error) {
-                console.error('Failed to publish', error);
-            }
-        });
-    }
+  // Handle connection errors
+  if (error) {
+    console.error('Failed to connect', error);
+  } else {
+    // Publish the publisher we initialzed earlier (this will trigger 'streamCreated' on other
+    // clients)
+    session.publish(publisher, function(error) {
+      if (error) {
+        console.error('Failed to publish', error);
+      }
+    });
+  }
 });
