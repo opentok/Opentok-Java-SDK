e978f468efdb0d6ee3518b6d7c56f424458cb07f
Jaideep Shah <jaideep@tokbox.com>
2018-09-18 12:16:29 -0700

    3272: first cut

---
+++ src/main/java/com/opentok/BroadcastLayout.java
@@ -0,0 +1,11 @@
+package com.opentok;
+
+public class BroadcastLayout extends ArchiveLayout {
+    public BroadcastLayout(Type type, String stylesheet) {
+        super(type, stylesheet);
+    }
+
+    public BroadcastLayout(Type type) {
+        super(type);
+    }
+}
