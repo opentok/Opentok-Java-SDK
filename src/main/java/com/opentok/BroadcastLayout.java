98d97426297b7efb2a7bb393a05af0b9606d94cb
Jaideep Shah <jaideep@tokbox.com>
2018-09-21 14:32:08 -0700

    MIT Lic

--- src/main/java/com/opentok/BroadcastLayout.java
+++ src/main/java/com/opentok/BroadcastLayout.java
@@ -1,3 +1,10 @@
+/**
+ * OpenTok Java SDK
+ * Copyright (C) 2018 TokBox, Inc.
+ * http://www.tokbox.com
+ *
+ * Licensed under The MIT License (MIT). See LICENSE file for more information.
+ */
 package com.opentok;
 
 public class BroadcastLayout extends ArchiveLayout {
