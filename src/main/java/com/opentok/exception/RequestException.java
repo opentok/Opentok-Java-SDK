2a4c8515761b50de46bcbf508257b30424aa9bad
Manik <msach22@gmail.com>
2019-01-12 18:23:25 +0000

    update license copyright date

--- src/main/java/com/opentok/exception/RequestException.java
+++ src/main/java/com/opentok/exception/RequestException.java
@@ -1,6 +1,6 @@
 /**
  * OpenTok Java SDK
- * Copyright (C) 2018 TokBox, Inc.
+ * Copyright (C) 2019 TokBox, Inc.
  * http://www.tokbox.com
  *
  * Licensed under The MIT License (MIT). See LICENSE file for more information.
