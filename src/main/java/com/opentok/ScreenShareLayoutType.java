b7a257ebdad4d66a0af1c898a78cb08c42be371e
slorello89 <slorello89@gmail.com>
2021-02-25 11:55:31 -0500

    adding licence to ScreenShareLayoutType.java

--- src/main/java/com/opentok/ScreenShareLayoutType.java
+++ src/main/java/com/opentok/ScreenShareLayoutType.java
@@ -1,3 +1,10 @@
+/**
+ * OpenTok Java SDK
+ * Copyright (C) 2021 Vonage.
+ * http://www.tokbox.com
+ *
+ * Licensed under The MIT License (MIT). See LICENSE file for more information.
+ */
 package com.opentok;
 
 import com.fasterxml.jackson.annotation.JsonValue;
