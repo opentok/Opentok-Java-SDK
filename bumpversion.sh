517287d4196f56249eb954f561776bef691fb57e
Sina Madani <sina.madani@vonage.com>
2023-01-13 16:43:41 +0000

    Update copyright year

--- bumpversion.sh
+++ bumpversion.sh
@@ -1,3 +1,10 @@
+#
+# OpenTok Java SDK
+# Copyright (C) 2023 Vonage.
+# http://www.tokbox.com
+#
+# Licensed under The MIT License (MIT). See LICENSE file for more information.
+#
 if [ "$1" = "" ]
 then
   echo "Usage: $0 <new version>"
