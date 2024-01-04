bec6665d94f48a403252eb9d4f3c11a96281b3e5
Sina Madani <sina.madani@vonage.com>
2024-01-04 09:36:43 +0000

    Bump copyright year

--- bumpversion.sh
+++ bumpversion.sh
@@ -1,6 +1,6 @@
 #
 # OpenTok Java SDK
-# Copyright (C) 2023 Vonage.
+# Copyright (C) 2024 Vonage.
 # http://www.tokbox.com
 #
 # Licensed under The MIT License (MIT). See LICENSE file for more information.
--- src/main/java/com/opentok/exception/package-info.java
+++ src/main/java/com/opentok/exception/package-info.java
@@ -1,8 +1,8 @@
 /**
  * OpenTok Java SDK
- * Copyright (C) 2023 Vonage.
+ * Copyright (C) 2024 Vonage.
  * http://www.tokbox.com
- *
+ * <p>
  * Licensed under The MIT License (MIT). See LICENSE file for more information.
  */
 /**
