de3ba7f261b88c0118275630438a48475f8360e5
Sina Madani <sina.madani@vonage.com>
2024-01-04 09:47:23 +0000

    Fix license headers

--- src/main/java/com/opentok/constants/DefaultUserAgent.java
+++ src/main/java/com/opentok/constants/DefaultUserAgent.java
@@ -2,7 +2,7 @@
  * OpenTok Java SDK
  * Copyright (C) 2024 Vonage.
  * http://www.tokbox.com
- * <p>
+ * 
  * Licensed under The MIT License (MIT). See LICENSE file for more information.
  */
 package com.opentok.constants;
