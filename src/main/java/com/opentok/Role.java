a46d335ceb24552a30e5bdfb5817a5b394d02644
Sina Madani <sina.madani@vonage.com>
2024-04-18 12:08:11 +0100

    feat: Add publisheronly role (#252)
    * Bump dependencies
    
    * Bump version: v4.13.2 → v4.14.0
    
    * Add publisheronly role

--- src/main/java/com/opentok/Role.java
+++ src/main/java/com/opentok/Role.java
@@ -6,30 +6,38 @@
  * Licensed under The MIT License (MIT). See LICENSE file for more information.
  */
 package com.opentok;
+
 /**
  * Defines values for the role parameter of the {@link TokenOptions.Builder#role(Role role)} method.
  */
 public enum Role {
     /**
-    *   A subscriber can only subscribe to streams.
+    * A subscriber can only subscribe to streams.
     */
     SUBSCRIBER,
+
     /**
     * A publisher can publish streams, subscribe to streams, and signal. (This is the default
     * value if you do not set a role by calling the {@link TokenOptions.Builder#role(Role role)}
     * method.
     */
     PUBLISHER,
+
     /**
     * In addition to the privileges granted to a publisher, a moderator can perform
     * moderation functions, such as forcing clients to disconnect, to stop publishing streams,
     * or to mute audio in published streams. See the
     * <a href="https://tokbox.com/developer/guides/moderation/">Moderation developer guide</a>.
     */
-    MODERATOR;
+    MODERATOR,
+
+    /**
+     * Publish only privilege.
+     */
+    PUBLISHER_ONLY;
 
     @Override
     public String toString() {
-        return super.toString().toLowerCase();
+        return name().toLowerCase().replace("_", "");
     }
 }
--- src/main/java/com/opentok/constants/Version.java
+++ src/main/java/com/opentok/constants/Version.java
@@ -8,5 +8,5 @@
 package com.opentok.constants;
 
 public class Version {
-    public static final String VERSION = "4.13.2";
+    public static final String VERSION = "4.14.0";
 }
