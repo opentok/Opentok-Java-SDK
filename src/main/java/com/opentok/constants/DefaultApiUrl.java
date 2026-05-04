715222fa6148561180e2e974a2b74cdbc0ae2f66
Chris Tankersley <chris@ctankersley.com>
2026-05-04 11:35:32 -0400

    chore: Update license headers to 2026 (#298)
    * chore: Update license headers to 2026, updated gradle and fixed build issues

--- src/main/java/com/opentok/constants/DefaultApiUrl.java
+++ src/main/java/com/opentok/constants/DefaultApiUrl.java
@@ -1,8 +1,8 @@
 /**
  * OpenTok Java SDK
- * Copyright (C) 2025 Vonage.
+ * Copyright (C) 2026 Vonage.
  * http://www.tokbox.com
- * 
+ *
  * Licensed under The MIT License (MIT). See LICENSE file for more information.
  */
 package com.opentok.constants;
